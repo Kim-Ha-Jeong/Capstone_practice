a6785c58a371fc1f9f4ae6e48db14fe2db5dbc21
Kim-Ha-Jeong <kha031821@gmail.com>
2020-10-04 13:13:26 +0900

    separate router

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+
 const db_config = require(__dirname+'/database.js');
 const conn = db_config.init();
+db_config.connect(conn);
 const bodyParser = require('body-parser');
 
-db_config.connect(conn);
+const indexRouter = require('./router/index');
+const userRouter = require('./router/user');
 
-app.use(express.json())
+app.use(express.json());
+app.set('views', __dirname+ '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
 
 app.listen(3000, () => {
-    console.log("Listening on port 3000...");
-})
+    console.log(`Listening on port 3000`);
+});
+
+app.use('/', indexRouter);
+app.use('/user', userRouter);
+
+module.exports = app;
