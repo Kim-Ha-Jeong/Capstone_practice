c91b2c5e01df36eb27e13ee198b4f1cc22aa7813
Kim-Ha-Jeong <kha031821@gmail.com>
2020-10-05 23:03:07 +0900

    user update

--- model/user.model.js
+++ model/user.model.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 const db = require('./db.js');
 const ejs = require('ejs');
-const file = __dirname+"/../views/user.html";
+const file = __dirname + "/../views/user.html";
+const updateFile = __dirname + "/../views/userUpdate.html";
 
 const User = (user) => {
     this.email = user.email;
@@ -13,21 +14,21 @@ const User = (user) => {
 User.create = (req, res) => {
     const sql = 'INSERT INTO user SET ?';
     const values = {
-        email: req.body.email, 
-        password: req.body.password, 
-        name: req.body.name, 
+        email: req.body.email,
+        password: req.body.password,
+        name: req.body.name,
         telephone: req.body.telephone
     }
     db.query(sql, values, (err, result) => {
-        if(!err) res.redirect('/user');
+        if (!err) res.redirect('/user');
         else console.log(err);
     })
 };
 
 User.getAll = (req, res) => {
-    fs.readFile( file, 'utf8', (error, data) => {
+    fs.readFile(file, 'utf8', (error, data) => {
         db.query("SELECT * FROM user", (err, result) => {
-            if(!err) res.send(ejs.render(data, {
+            if (!err) res.send(ejs.render(data, {
                 data: result
             }));
             else console.log(err);
@@ -36,9 +37,37 @@ User.getAll = (req, res) => {
 };
 
 User.findById = (req, res) => {
-    db.query("SELECT * FROM user WHERE id=?", [req.params.id], 
-    (err, result) => {
-        if(!err) res.send(result);
+    db.query("SELECT * FROM user WHERE id=?", [req.params.id],
+        (err, result) => {
+            if (!err) res.send(result);
+            else console.log(err);
+        })
+}
+
+User.update = (req, res) => {
+    const sql = "SELECT * FROM user WHERE id = ?";
+    fs.readFile(updateFile, 'utf8', (error, data) => {
+        db.query(sql, [req.params.id], (err, result) => {
+            if (!err) {
+                res.send(ejs.render(data, {
+                    data: result[0]
+                }));
+            }
+            else console.log(err);
+        });
+    });
+};
+
+User.updateById = (req, res) => {
+    const sql = "UPDATE user SET ? WHERE id=?";
+    const values = {
+        email: req.body.email,
+        password: req.body.password,
+        name: req.body.name,
+        telephone: req.body.telephone
+    }
+    db.query(sql, [values, req.params.id], (err, result) => {
+        if (!err) res.redirect('/user');
         else console.log(err);
     })
 }
--- router/user.js
+++ router/user.js
@@ -5,11 +5,14 @@ const user = require('../model/user.model');
 router.get('/', user.getAll);
 
 router.post('/create', user.create);
-
 router.get('/create', (req, res, next) => {
     res.render('userCreate.html');
 });
 
 router.get('/:id', user.findById);
+
+router.post('/update/:id', user.updateById);
+router.get('/update/:id', user.update);
+
 
 module.exports = router;
